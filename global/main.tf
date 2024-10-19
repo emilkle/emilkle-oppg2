3fa4989ea15f74749d3469f30451e63ddfd21eef
Emil <emilkle@stud.ntnu.no>
2024-10-19 16:35:48 +0200

    Added remote state storage and terraform provider block to the main.tf file in global.

--- global/main.tf
+++ global/main.tf
@@ -1,1 +1,19 @@
-## Test commit 7
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.0.1"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-backend-eks"
+    storage_account_name = "sabackendekszqreqedcf9"
+    container_name       = "tfstate"
+    key                  = "opera-terra-web-app.tfstate"
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "3291a0d9-96b5-41ee-9b93-5b28b419919f"
+  features {}
+}
