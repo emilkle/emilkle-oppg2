45ce97ab521bcd046896c020871120f138a06a19
Emil <emilkle@stud.ntnu.no>
2024-10-20 18:58:50 +0200

    Added mssql database and modules to the core_infrastructure main.tf file

--- core_infrastructure/main.tf
+++ core_infrastructure/main.tf
@@ -17,3 +17,32 @@ provider "azurerm" {
   subscription_id = "3291a0d9-96b5-41ee-9b93-5b28b419919f"
   features {}
 }
+
+resource "azurerm_resource_group" "infra-rg" {
+  name     = var.rgname
+  location = var.location
+}
+
+module "networking" {
+  source = "../modules/networking"
+}
+
+module "app_service" {
+  source = "../modules/app_service"
+}
+
+module "database" {
+  source = "../modules/database"
+  rgname = azurerm_resource_group.infra-rg.name
+  location = azurerm_resource_group.infra-rg.location
+  mssqldbserevername = var.mssqldbserevername
+  mssqldbname = var.mssqldbname
+}
+
+module "storage" {
+  source = "../modules/storage"
+}
+
+module "nn" {
+  source = "../modules/nn"
+}
---
+++ modules/database/main.tf
@@ -0,0 +1,23 @@
+resource "azurerm_mssql_server" "mssql-e-commerce-db-server" {
+  name                         = var.mssqldbserevername
+  resource_group_name          = var.rgname
+  location                     = var.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_mssql_database" "mssql-e-commerce-db" {
+  name         = var.mssqldbname
+  server_id    = azurerm_mssql_server.mssql-e-commerce-db-server.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "S0"
+  enclave_type = "VBS"
+
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = true
+  }
+}
