6045911bb5c16bd1aa7d5c14be06dbedc115e602
Emil <emilkle@stud.ntnu.no>
2024-10-22 18:31:34 +0200

    Added resource group to global module.

--- global/main.tf
+++ global/main.tf
@@ -1,19 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.0.1"
-    }
-  }
-  backend "azurerm" {
-    resource_group_name  = "rg-backend-eks"
-    storage_account_name = "sabackendekszqreqedcf9"
-    container_name       = "tfstate"
-    key                  = "opera-terra/web-app/global/global.tfstate"
-  }
-}
-
-provider "azurerm" {
-  subscription_id = "3291a0d9-96b5-41ee-9b93-5b28b419919f"
-  features {}
+resource "azurerm_resource_group" "infra-rg" {
+  name     = var.rgname
+  location = var.location
 }
